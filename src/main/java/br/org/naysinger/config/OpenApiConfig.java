9c90618e36329d3a6921797ebd0d9e83e10c6e87
Maike Naysinger Borges <maikenborges@gmail.com>
2025-03-13 00:00:00 -0300

    merge

--- src/main/java/br/org/naysinger/config/OpenApiConfig.java
+++ src/main/java/br/org/naysinger/config/OpenApiConfig.java
@@ -3,26 +3,18 @@ package br.org.naysinger.config;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import io.swagger.v3.oas.models.servers.Server;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-
-import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
     @Bean
-    public OpenAPI openAPI(ApplicationContext applicationContext) {
-        String contextPath = applicationContext.getEnvironment().getProperty("server.servlet.context-path", "/");
-        Server server = new Server().url(contextPath);
-
+    public OpenAPI openAPI() {
         return new OpenAPI()
                 .info(new Info().title("Storage Service")
                         .description(
                                 "Reactive API of Storage Service")
                         .version("v1.0")
-                        .license(new License().name("Apache 2.0").url("http://springdoc.org")))
-                .servers(List.of(server));
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
     }
 }
